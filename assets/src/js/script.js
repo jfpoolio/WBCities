c48ee8a0a839aadb03cae7385dd45745e5d9fb34
jfpBucket <jfpoolio@gmail.com>
2018-07-22 00:29:47 -0400

    Initial commit

---
+++ assets/src/js/script.js
@@ -0,0 +1,29 @@
+(function($) {
+    "use strict";
+
+    var w = {};
+
+    w.width = $(window).width();
+
+    var wb = {
+        exampleScript: {
+            test: function() {
+
+            },
+            run: function() {
+
+            }
+        }
+    };
+
+    function runObject() {
+        for (var key in wb) {
+            if (wb[key].test()) {
+                wb[key].run();
+            }
+        }
+    }
+
+    runObject();
+
+}(jQuery));
